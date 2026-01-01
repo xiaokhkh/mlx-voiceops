c08b7870521c4d58a12d7cc52dbca74122c87a73
konh <aminer@Mac.bbrouter>
2026-01-01 21:14:49 +0800

    Fix hotkey build

--- apps/macos/VoiceOps/AppMain.swift
+++ apps/macos/VoiceOps/AppMain.swift
@@ -1,4 +1,5 @@
 import AppKit
+import Carbon
 import Combine
 import SwiftUI
 
@@ -13,6 +14,7 @@ struct VoiceOpsApp: App {
     }
 }
 
+@MainActor
 final class AppDelegate: NSObject, NSApplicationDelegate {
     private var statusItem: NSStatusItem?
     private var panel: OverlayPanel?
